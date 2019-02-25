848f734edf25176622a809bf5646ad054c082b1a
Jon Proulx <jon@jonproulx.com>
2019-02-25 14:49:17 -0500

    simplest ec2 example

---
+++ simplest-ec2/main.tf
@@ -0,0 +1,28 @@
+####################################################################
+#
+# This actually defines and starts the VM using variable from vars.tf
+# you should read (and probably edit) that file first
+#
+######################################################################
+
+# with this commented out terraform will use the "default" identity in
+# ~/.aws/credentials , this would set it to use an alternate file
+# with "terraform" idetity
+provider "aws" {
+  region                  = "us-east-2"
+#  shared_credentials_file = "../common/aws_cred.ini"
+#  profile                 = "terraform"
+  version                 = "~> 1.60"
+}
+
+resource "aws_instance" "terraform_test" {
+  ami = "${data.aws_ami.ubuntu_bionic.id}"
+  instance_type = "${var.instance_type}"
+  user_data   = "${var.cloudcfg}"
+
+  tags {
+    Name = "${var.servername}"
+  }    
+  security_groups = "${var.security_groups}"
+
+}
---
+++ simplest-ec2/outputs.tf
@@ -0,0 +1,8 @@
+# since we're using dynamic addressing we shoudl define and output
+# for the instance IP see https://www.terraform.io/docs/configuration/outputs.html
+output "private_ip" {
+  value = "${aws_instance.terraform_test.private_ip}"
+}
+output "public_ip" {
+  value = "${aws_instance.terraform_test.public_ip}"
+}
